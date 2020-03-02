e42a3332db8143bf5d6d8a42d3cd5d6db6962b1b
mr-font <mrfont@miners.utep.edu>
2020-03-02 14:53:47 -0700

    3/2/2020 Started project.

---
+++ Java/src/SortOfSort.java
@@ -0,0 +1,19 @@
+public class SortOfSort {
+
+
+
+
+    public  static  void sortOfSort(int[] unsortedArray){
+
+        int temp = 0;
+        int largestNum = unsortedArray[0];
+
+        for(int i = 0; i < unsortedArray.length; ++i){
+
+            if(unsortedArray[i] > largestNum){
+
+
+            }
+        }
+    }
+}
