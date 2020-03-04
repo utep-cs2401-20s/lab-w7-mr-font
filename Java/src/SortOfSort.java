f032fb7719a0087041979fab8221f75d4311033c
mr-font <mrfont@miners.utep.edu>
2020-03-04 14:49:42 -0700

    3/4/2020 Started helper method

--- Java/src/SortOfSort.java
+++ Java/src/SortOfSort.java
@@ -5,15 +5,27 @@ public class SortOfSort {
 
     public  static  void sortOfSort(int[] unsortedArray){
 
+        int sortedOffsetRight = 1;
+        int sortedOffsetLeft = 0;
+        int offSet = 2;
+        int loopNum = 0;
         int temp = 0;
-        int largestNum = unsortedArray[0];
 
-        for(int i = 0; i < unsortedArray.length; ++i){
+        //false = left, true = right
+        boolean leftOrRight;
+        int largestNum = unsortedArray[unsortedArray.length-1];
+
+        for(int i = 0; i < unsortedArray.length-1; ++i){
 
             if(unsortedArray[i] > largestNum){
-
+                temp = largestNum;
 
             }
         }
+    }
+
+    public int[] sortOfHelper(int[] unsortedArray, boolean leftRight, ){
+
+
     }
 }
