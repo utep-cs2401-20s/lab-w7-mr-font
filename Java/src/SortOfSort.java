00773664917564eb98b2b51b8a959b8ae900ad41
mr-font <mrfont@miners.utep.edu>
2020-03-05 23:29:28 -0700

    3/5/2020 @ 11:28pm backup before trying a rewrite of SortOfSort

--- Java/src/SortOfSort.java
+++ Java/src/SortOfSort.java
@@ -7,24 +7,77 @@ public class SortOfSort {
 
         int sortedOffsetRight = 1;
         int sortedOffsetLeft = 0;
+        int rightOffSetMultiplier = 1;
+        int leftOffSetMultiplier = 1;
         int offSet = 2;
         int loopNum = 0;
-        int temp = 0;
+        //int temp = 0;
 
         //false = left, true = right
-        boolean leftOrRight;
-        int largestNum = unsortedArray[unsortedArray.length-1];
+        boolean leftOrRight = true;
 
-        for(int i = 0; i < unsortedArray.length-1; ++i){
+        //int largestNum = unsortedArray[unsortedArray.length-1];
 
-            if(unsortedArray[i] > largestNum){
-                temp = largestNum;
+        while(sortedOffsetLeft <= ((unsortedArray.length-1)/ 2) ){
 
+
+            if(leftOrRight){
+                loopNum = 0;
+                for(int i = unsortedArray.length-1; i >= (sortedOffsetLeft * leftOffSetMultiplier)-1; --i){
+
+                    int largestNum = i;
+
+                    for(int j = i - 1; j >= sortedOffsetLeft; --j){
+
+                        if(unsortedArray[j] > largestNum){
+                            largestNum = j;
+                        }
+                    }
+
+                    if(unsortedArray[largestNum] != i){
+                        int temp = unsortedArray[largestNum];
+
+                        unsortedArray[largestNum] = unsortedArray[i];
+                        unsortedArray[i] = temp;
+
+                    }
+                    loopNum ++;
+                    rightOffSetMultiplier++;
+                    if(loopNum == 2){
+                        leftOrRight = false;
+                    }
+                }
+            }else{
+                loopNum = 0;
+                sortedOffsetLeft = 1;
+                for(int i = 0; i < (sortedOffsetRight * rightOffSetMultiplier); ++i){
+
+                    int largestNum = i;
+
+                    for(int j = i - 1; j >= sortedOffsetLeft; --j){
+
+                        if(unsortedArray[j] > largestNum){
+                            largestNum = j;
+                        }
+                    }
+
+                    if(unsortedArray[largestNum] != i){
+                        int temp = unsortedArray[largestNum];
+
+                        unsortedArray[largestNum] = unsortedArray[i];
+                        unsortedArray[i] = temp;
+
+                    }
+                    loopNum ++;
+                    leftOffSetMultiplier++;
+                    if(loopNum == 2){
+                        leftOrRight = true;
+                    }
             }
         }
     }
 
-    public int[] sortOfHelper(int[] unsortedArray, boolean leftRight, ){
+    public int[] sortOfHelper(int[] unsortedArray, boolean leftRight, int offsetMultiplier){
 
 
     }
